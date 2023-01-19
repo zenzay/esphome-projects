61ba52ed989d3e739003ac0f05e113695cf5d129
zenzay <31970501+zenzay@users.noreply.github.com>
2023-01-19 16:48:03 +0100

    Update amg883_detection.h

--- thermo-sensor/amg883_detection.h
+++ thermo-sensor/amg883_detection.h
@@ -21,8 +21,8 @@ public:
     {
         float pixel_temp;
         float min_temp = 0;
-		int hot_count = 0;
-		// run trough grid and find minimum temperature
+	int hot_count = 0;
+	// run trough grid and find minimum temperature
         for (unsigned char i = 0; i < total_pixels; i++)
         {
             pixel_temp = grideye.getPixelTemperature(i);
@@ -31,18 +31,18 @@ public:
                 min_temp = pixel_temp;
             }
         }
-		// run through the grid again to count number of hot pixels
+	// run through the grid again to count number of hot pixels
         for (unsigned char i = 0; i < total_pixels; i++)
         {
             pixel_temp = grideye.getPixelTemperature(i);
-			// adjust the hardcoded difference of 3, if needed, to get higher or lower hot pixel counts
+	    // adjust the hardcoded difference of 3, if needed, to get higher or lower hot pixel counts
             if (pixel_temp > (min_temp + 3))
             {
                 hot_count++;
             }
         }
 
-		// adjust the minimum hot count to fit your case (use the hot_pixels component to find that)
+	// adjust the minimum hot count to fit your case (use the hot_pixels component to find that)
         if (hot_count > 7)
         {
             if (!last_state)
